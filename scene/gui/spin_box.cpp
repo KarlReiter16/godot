e45a14e84738df978da6566fb6c77099898483e5
KarlReiter16 <karl.reiter@ufl.edu>
2025-02-17 01:59:40 -0500

    Fix style_box consistency for down_hover_stylebox
    change all down_hover_stylebox to down_hover_style_box

--- scene/gui/spin_box.cpp
+++ scene/gui/spin_box.cpp
@@ -386,7 +386,7 @@ void SpinBox::_notification(int p_what) {
 				down_icon = theme_cache.down_pressed_icon;
 				down_icon_modulate = theme_cache.down_pressed_icon_modulate;
 			} else if (state_cache.down_button_hovered && !drag.enabled) {
-				down_style_box = theme_cache.down_hover_stylebox;
+				down_style_box = theme_cache.down_hover_style_box;
 				down_icon = theme_cache.down_hover_icon;
 				down_icon_modulate = theme_cache.down_hover_icon_modulate;
 			}
@@ -604,7 +604,7 @@ void SpinBox::_bind_methods() {
 	BIND_THEME_ITEM_CUSTOM(Theme::DATA_TYPE_STYLEBOX, SpinBox, up_pressed_style_box, "up_background_pressed");
 	BIND_THEME_ITEM_CUSTOM(Theme::DATA_TYPE_STYLEBOX, SpinBox, up_disabled_style_box, "up_background_disabled");
 	BIND_THEME_ITEM_CUSTOM(Theme::DATA_TYPE_STYLEBOX, SpinBox, down_base_style_box, "down_background");
-	BIND_THEME_ITEM_CUSTOM(Theme::DATA_TYPE_STYLEBOX, SpinBox, down_hover_stylebox, "down_background_hovered");
+	BIND_THEME_ITEM_CUSTOM(Theme::DATA_TYPE_STYLEBOX, SpinBox, down_hover_style_box, "down_background_hovered");
 	BIND_THEME_ITEM_CUSTOM(Theme::DATA_TYPE_STYLEBOX, SpinBox, down_pressed_stylebox, "down_background_pressed");
 	BIND_THEME_ITEM_CUSTOM(Theme::DATA_TYPE_STYLEBOX, SpinBox, down_disabled_stylebox, "down_background_disabled");
 
--- scene/gui/spin_box.h
+++ scene/gui/spin_box.h
@@ -108,7 +108,7 @@ class SpinBox : public Range {
 		Ref<StyleBox> up_pressed_style_box;
 		Ref<StyleBox> up_disabled_style_box;
 		Ref<StyleBox> down_base_style_box;
-		Ref<StyleBox> down_hover_stylebox;
+		Ref<StyleBox> down_hover_style_box;
 		Ref<StyleBox> down_pressed_stylebox;
 		Ref<StyleBox> down_disabled_stylebox;
 
